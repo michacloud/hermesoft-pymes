997ca9453eed19e942ce47d6c83c86f9d42380c3
MICHA CLOUD TECHNOLOGY <83597106+michacloud@users.noreply.github.com>
2023-01-23 13:10:55 -0400

    Add files via upload

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+define('FS_COOKIES_EXPIRE', 31536000);
+define('FS_ROUTE', '/factura');
+define('FS_DB_FOREIGN_KEYS', true);
+define('FS_DB_TYPE_CHECK', true);
+define('FS_MYSQL_CHARSET', 'utf8');
+define('FS_MYSQL_COLLATE', 'utf8_bin');
+define('FS_LANG', 'es_DO');
+define('FS_TIMEZONE', 'America/Santo_Domingo');
+define('FS_DB_TYPE', 'postgresql');
+define('FS_DB_HOST', '104.236.3.94');
+define('FS_DB_PORT', '5432');
+define('FS_DB_NAME', 'michacloud_db');
+define('FS_DB_USER', 'micha_pyme');
+define('FS_DB_PASS', '4str0n4Ut4');
+define('FS_HIDDEN_PLUGINS', '');
+define('FS_DEBUG', false);
+define('FS_DISABLE_ADD_PLUGINS', false);
+define('FS_DISABLE_RM_PLUGINS', false);
+
